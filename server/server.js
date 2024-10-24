e28343ea789b57020060d92a7e5447deca672d39
Rabia Zubair <rabiazubair949@gmail.com>
2024-10-24 14:07:52 +0500

    commit

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use(errorMiddleware);
 app.use(express.static(path.join(__dirname, "..", "/client/dist")));
 console.log(__dirname);
 app.get("*", (_, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "..", "dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, "..", "client", "dist", "index.html"));
 });
 
 connectDb().then(() => {
