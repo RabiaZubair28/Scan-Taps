d490bec6569518ba54721e0c4b566140ec3a40d0
Rabia Zubair <rabiazubair949@gmail.com>
2024-10-25 16:29:00 +0500

    commit

--- client/src/pages/Client.jsx
+++ client/src/pages/Client.jsx
@@ -396,17 +396,7 @@ img10;
         );
       }
       else{
-        return (
-          <div className=" content">
-        <h2 className="header" style={{ color:"white", textAlign:"center"}}>404</h2>
-        <h4 style={{ color:"white", textAlign:"center"}}>Sorry! Page not found</h4>
-        <p style={{ color:"white", textAlign:"center"}}>
-          Oops! It seems like the page you're trying to access doesn't exist.
-          If you believe there's an issue, feel free to report it, and we'll
-          look into it.
-        </p>
-      </div>
-        );
+        return;
       }
       }
 
