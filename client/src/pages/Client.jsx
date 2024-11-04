78cf4e90d7fcb132ee7da711fe2c0a8d78f8098d
Rabia Zubair <rabiazubair949@gmail.com>
2024-11-04 10:52:51 +0500

    commit

--- client/src/pages/Client.jsx
+++ client/src/pages/Client.jsx
@@ -241,10 +241,11 @@ const currentPageUrl = window.location.href;
                   <img src={eye} height={25}></img>
                   <span style={{display:"flex",alignItems:"center",justifyContent:"center",
                   }}>&nbsp;{visitCount} &nbsp;&nbsp;&nbsp;&nbsp;</span>
+                  <div style={{backgroundColor:"rgb(22, 33, 92)"}}>
                   <FaDownload size={20} onClick={downloadContactCard} color="white" />
                   <span style={{display:"flex",alignItems:"center",color:"white",justifyContent:"center"
                   }} onClick={downloadContactCard} >&nbsp;&nbsp;Save Info</span>
-          
+                </div>
                 </div>
                 <div className="info">
                 { (clientName!= "") &&<div className="detail02">{clientName}</div>}
