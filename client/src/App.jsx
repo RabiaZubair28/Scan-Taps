940e098fa19f1f41fb0e7921a62fc611119088d5
Rabia Zubair <rabiazubair949@gmail.com>
2024-11-10 15:37:18 +0500

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Client }  from "./pages/Client.jsx"
 // import { Signin } from "./pages/Signin.jsx"
 import { Error } from "./components/Error.jsx"
-import { User }  from "./pages/User.jsx"
-import { Portal }  from "./pages/Portal.jsx"
+// import { User }  from "./pages/User.jsx"
+// import { Portal }  from "./pages/Portal.jsx"
 import { Signin } from "./pages/Signin.jsx"
 import "./App.css"
 
@@ -22,8 +22,8 @@ export const App = () => {
         <Route path="/signin" element={<Signin />}></Route> */}
         <Route path="/signin" element={<Signin />}></Route> 
         <Route path="/:name" element={<Client />}></Route>
-        <Route path="/client/:id" element={<User />}></Route>
-        <Route path="/portal/:username/:password" element={<Portal />}></Route>
+        {/* <Route path="/client/:id" element={<User />}></Route>
+        <Route path="/portal/:username/:password" element={<Portal />}></Route> */}
         <Route path="*" element={<Error />}></Route>
 
       </Routes>
