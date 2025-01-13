604560c3a1add9a0ceb31e94c792c8afe941ecd8
Rabia Zubair <rabiazubair949@gmail.com>
2025-01-13 16:10:15 +0500

    commit

--- client/src/pages/Client.jsx
+++ client/src/pages/Client.jsx
@@ -265,7 +265,7 @@ const currentPageUrl = window.location.href;
           <div>
                 <div className="eng-heading">{name}</div>
               
-                { (romanName != "") && <div className="roman-heading">{romanName}</div>}
+                { (romanName != "") && <div className="roman-heading" style={{whiteSpace: "pre-wrap"}}>{romanName}</div>}
   
                 <div className="eye">
                   <img src={eye} height={25}></img>
