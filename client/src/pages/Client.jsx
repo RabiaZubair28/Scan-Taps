fadccce7e550587edfacaf8215aa67ce6056dad2
Rabia Zubair <rabiazubair949@gmail.com>
2025-01-30 13:47:38 +0500

    commit

--- client/src/pages/Client.jsx
+++ client/src/pages/Client.jsx
@@ -69,6 +69,8 @@ designation,
 whatsapp01,
 whatsapp02,
 instagramLink,
+instagramLink02,
+instagramLink03,
 snapchatLink,
 youtubeLink,
 tiktokLink,
@@ -327,7 +329,7 @@ const currentPageUrl = window.location.href;
                 <p className="details">{whatsapp02}</p>
               </div>
             </div></a>}
-            { (instagramLink != "") &&<a href={`${instagramLink}`} className="link"><div className="socials">
+            { (instagramLink != "" || instagramLink02 != "" || instagramLink03 != "") &&<a href={`${instagramLink}`} className="link"><div className="socials">
               <img src={insta} height={50} width={50}></img>
               <div>
                 <p className="tag-heading">Instagram { (flag!= true) &&<span> / انستغرام</span>}</p>
